c8a802237a6ceff8e08b9976b3b3c8db7d1c5b81
carolduong42 <86270108+carolduong42@users.noreply.github.com>
2021-06-22 17:03:03 -0700

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+#print('Hello World!')
+
+name = 'LeBron' # string
+age = 35 # Integer 
+salary = 37.44 # float
+plays_basketball = True #boolean
+jersey_number = '23' # string
+
+# STEP 2
+car_name = 'Ford'
+X = 50 
+my_first_name = 'Carol'
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
+print(type(car_name))
+print(type(X))
+print(type(jersey_number))
